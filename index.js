543538749c2f7e3c143283a466ccfa31077c09e4
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-06-26 17:21:53 +0300

    adds scope lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Bob";
+}
+const leastFavoriteCustomer = "Alice";
